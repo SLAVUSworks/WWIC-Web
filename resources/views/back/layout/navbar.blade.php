e216d9b55ed9d8af6fee4a7b51ac9c0efae03ae6
SLAVUSworks <abdurrahmanagharabbani45@gmail.com>
2024-03-29 17:02:47 +0700

    BE Dasar

---
+++ resources/views/back/layout/navbar.blade.php
@@ -0,0 +1,35 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3 sidebar-sticky">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">
+            <span data-feather="home" class="align-text-bottom"></span>
+            Dashboard
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span data-feather="file" class="align-text-bottom"></span>
+            Artikel
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span data-feather="list" class="align-text-bottom"></span>
+            Kategori
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span data-feather="users" class="align-text-bottom"></span>
+            Users
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span data-feather="log-out" class="align-text-bottom"></span>
+            Logout
+          </a>
+        </li>
+      </ul>
+  </nav>
---
+++ resources/views/back/layout/template.blade.php
@@ -0,0 +1,51 @@
+ {{-- Head --}}
+
+ <!doctype html>
+ <html lang="en">
+   <head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="description" content="">
+     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+     <meta name="generator" content="Hugo 0.104.2">
+     <title>Ini Dashboard</title>
+ 
+     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/dashboard/">
+ 
+     
+ 
+     
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     <!-- Custom styles for this template -->
+     <link href="{{ asset('back/css/dashboard.css') }}" rel="stylesheet">
+   </head>
+   <body>
+     
+ <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+   <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">WWIC - Under Development</a>
+   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+   <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
+   <div class="navbar-nav">
+     <div class="nav-item text-nowrap">
+       <a class="nav-link px-3" href="#">Sign out</a>
+     </div>
+   </div>
+ </header>
+ 
+ <div class="container-fluid">
+   <div class="row">
+    @include('back.layout.navbar')
+
+         {{-- Main --}}
+
+    @yield('main')
+
+         {{-- Footer --}}
+
+    <script src="/docs/5.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="{{ asset('back/js/dashboard.js') }}"></script>
+</body>
+</html>
