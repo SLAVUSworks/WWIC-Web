68e555e1b233dd22731962966e8ea178b4875d0f
SLAVUSworks <abdurrahmanagharabbani45@gmail.com>
2024-07-17 19:25:26 +0700

    Bring back the Config and Missing Bootstrap CDN

--- resources/views/front/layout/template.blade.php
+++ resources/views/front/layout/template.blade.php
@@ -15,6 +15,7 @@
         <link href="{{ asset('front/css/styles.css') }}" rel="stylesheet" />
         <link href="{{ asset('front/css/custom.css') }}" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
         @stack('css')
     </head>
